c39df2568269f19f4254f3d35af3c550506f7b06
JoshiiMaru <joshua2036david@gmail.com>
2026-06-03 09:56:15 -0500

    Export Cors Origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://clinica-frontend-v1.vercel.ap',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
