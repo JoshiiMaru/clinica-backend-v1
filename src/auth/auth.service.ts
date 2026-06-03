534549433864345a527ddf7f3f905f8ab8853027
JoshiiMaru <joshua2036david@gmail.com>
2026-06-03 08:56:44 -0500

    Export

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Usuario } from './entities/usuario.entity';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
